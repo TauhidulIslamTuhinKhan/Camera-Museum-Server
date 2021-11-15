290245f2f67c8a6371e395b39b32dac65dcf1562
TauhidulIslamTuhinKhan <tuhin0411@gmail.com>
2021-11-15 23:57:49 +0600

    done

--- index.js
+++ index.js
@@ -136,7 +136,9 @@ async function run() {
 
 run().catch(console.dir);
 
-
+app.get('/hello', (req, res) => {
+    res.send('Hello updated here');
+})
 
 app.get('/', (req, res) => {
     res.send('Hello Nobarun Camera Museum!')
